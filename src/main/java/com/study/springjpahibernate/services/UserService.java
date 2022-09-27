cc9d96be991104e3cbbdc78a9c82583a8c7f30e5
Caio Augusto <caioaugusto4520@gmail.com>
2022-09-27 20:46:03 -0300

    Service layer and component registration

---
+++ src/main/java/com/study/springjpahibernate/services/UserService.java
@@ -0,0 +1,25 @@
+package com.study.springjpahibernate.services;
+
+import com.study.springjpahibernate.entities.User;
+import com.study.springjpahibernate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
