0d09d2645b9751d755a423bb49b9f5c43f632240
Caio Augusto <caioaugusto4520@gmail.com>
2022-09-27 23:53:42 -0300

    Payment, one-to-one association

--- src/main/java/com/study/springjpahibernate/config/TestConfig.java
+++ src/main/java/com/study/springjpahibernate/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.study.springjpahibernate.config;
 import com.study.springjpahibernate.entities.Category;
 import com.study.springjpahibernate.entities.Order;
 import com.study.springjpahibernate.entities.OrderItem;
+import com.study.springjpahibernate.entities.Payment;
 import com.study.springjpahibernate.entities.Product;
 import com.study.springjpahibernate.entities.User;
 import com.study.springjpahibernate.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
     }
 }
--- src/main/java/com/study/springjpahibernate/entities/Order.java
+++ src/main/java/com/study/springjpahibernate/entities/Order.java
@@ -3,6 +3,7 @@ package com.study.springjpahibernate.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.study.springjpahibernate.entities.enums.OrderStatus;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
@@ -84,6 +86,14 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     public Set<OrderItem> getItems() {
         return items;
     }
@@ -91,6 +101,9 @@ public class Order implements Serializable {
     public void setItems(Set<OrderItem> items) {
         this.items = items;
     }
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     @Override
     public boolean equals(Object o) {
